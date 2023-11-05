6360b13dff1a6cb48dd9930e13d69924dd308876
RafiHul <rafihul2905@gmail.com>
2023-11-05 20:20:35 +0700

    done

--- core/main.py
+++ core/main.py
@@ -1,8 +1,14 @@
 x = input()
-xs = x.split()
-xs2 = xs[0].split(":")
-if xs[1] == "AM":
-    if xs[0] == "00:00":
-        print(xs[0])
-    if int(xs2[0]) > 0:
-        print("tes")
+xrep = x.replace(":"," ")
+xs = xrep.split(" ")
+if xs[2] == "AM":
+    if xs[0] == "00" and int(xs[0]) <= 11 and 0 <= int(xs[1]) <= 59:
+        print(f"00:{xs[1]}")
+    elif 00 <= int(xs[1]) <= 59 and int(xs[0]) <= 11:
+        print(f"{xs[0]}:{xs[1]}")
+if xs[2] == "PM":
+    if xs[0] == "12":
+        print(f"12:{xs[1]}")
+    elif 1 <= int(xs[0]) <= 11:
+        total = int(xs[0]) + 12
+        print(f"{total}:{xs[1]}")
